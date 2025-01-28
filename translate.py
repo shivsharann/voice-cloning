ae0dbeae96ac96a111908477683eab7d30216733
Netyam Shivsaran <155980006+shivsharann@users.noreply.github.com>
2025-01-28 18:31:59 +0530

    translate.py

--- translate.py
+++ translate.py
@@ -1,4 +1,3 @@
-from encoder.params_model import model_embedding_size as speaker_embedding_size
 from utils.argutils import print_args
 from synthesizer.inference import Synthesizer
 from encoder import inference as encoder
@@ -28,22 +27,7 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
     print_args(args, parser)
-        
-    if args.textin:
-        print("\nargs.textin: ", args.textin)
-    else:
-        print("\nNO VALUE: args.textin")   
-        
-    if args.voicein:
-        print("\nargs.voicein: ", args.voicein)
-    else:
-        print("\nNO VALUE: args.voicein")       
 
-    if args.out:
-        print("\nargs.out: ", args.out)
-    else:
-        print("\nNO VALUE: args.out")         
-    
     ## Print some environment information (for debugging purposes)
     print("Running a test of your configuration...\n")
     if not torch.cuda.is_available():
@@ -63,13 +47,11 @@ if __name__ == '__main__':
            gpu_properties.minor,
            gpu_properties.total_memory / 1e9))
     
-    
     ## Load the models one by one.
     print("Preparing the encoder, the synthesizer and the vocoder...")
     encoder.load_model(args.enc_model_fpath)
     synthesizer = Synthesizer(args.syn_model_dir.joinpath("taco_pretrained"))
     vocoder.load_model(args.voc_model_fpath)
-
 
     # ********************************
     if args.voicein:
@@ -81,12 +63,9 @@ if __name__ == '__main__':
         in_fpath = Path(input(message).replace("\"", "").replace("\'", ""))
 
     print("in_fpath: ", in_fpath)
-    
 
     embeds = []
-    embeds.append(encoder.embed_utterance(encoder.preprocess_wav("input.wav")))
-
-
+    embeds.append(encoder.embed_utterance(encoder.preprocess_wav(in_fpath)))
 
     print("Interactive generation loop")
 
@@ -97,28 +76,18 @@ if __name__ == '__main__':
         else:
             text = input("Write a sentence (+-20 words) to be synthesized:\n")
 
-        
-        # The synthesizer works in batch, so you need to put your data in a list or numpy array
         texts = [text]
-        #embeds = [embed]
-        # If you know what the attention layer alignments are, you can retrieve them here by
-        # passing return_alignments=True
         specs = synthesizer.synthesize_spectrograms(texts, embeds)
         spec = specs[0]
         print("Created the mel spectrogram")
         
-        
         ## Generating the waveform
         print("Synthesizing the waveform:")
-        # Synthesizing the waveform is fairly straightforward. Remember that the longer the
-        # spectrogram, the more time-efficient the vocoder.
         generated_wav = vocoder.infer_waveform(spec)
-        
+
         ## Post-generation
-        # There's a bug with sounddevice that makes the audio cut one second earlier, so we
-        # pad it.
         generated_wav = np.pad(generated_wav, (0, synthesizer.sample_rate), mode="constant")
-        
+
         # Save it on the disk
         if args.out:
             fpath = args.out
@@ -126,11 +95,9 @@ if __name__ == '__main__':
             fpath = "output.wav"
 
         print("generated_wav.dtype: ", generated_wav.dtype)
-
         librosa.output.write_wav(fpath, generated_wav.astype(np.float32), synthesizer.sample_rate)
 
         print("\nSaved output as %s\n\n" % fpath)
-        
         
     except Exception as e:
         print("Caught exception: %s" % repr(e))
